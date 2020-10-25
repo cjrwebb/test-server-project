9ce8f84db7fcf6de05ad99102e89da6d2c1cecbb
cjrwebb <66517368+cjrwebb@users.noreply.github.com>
2020-10-25 10:46:12 +0000

    Updated with moose data

--- app.R
+++ app.R
@@ -9,40 +9,62 @@
 
 library(shiny)
 library(tidyverse)
+library(janitor)
+library(ggimage)
 
-# Define UI for application that draws a histogram
+# Fundy National Park Moose Data
+moose_data <- read_csv("moose_data.csv")
+
+moose_data <- moose_data %>%
+    janitor::clean_names() %>%
+    slice(-1) %>%
+    mutate(total_count = as.numeric(total_count)) %>% 
+    group_by(year) %>%
+    summarise(total_count = sum(total_count, na.rm = TRUE))
+
+# moose_data %>%
+#     ggplot() +
+#     geom_bar(aes(x = year, y = total_count), stat = "identity") +
+#     ggimage::geom_image(aes(x = year, y = total_count + 4, image = "moose.png"), size = 0.15) +
+#     ylab("Number of Moose Spotted") +
+#     xlab("Year of Moose Survey") +
+#     ggtitle("How many moose seen in Fundy National Park, Canada") +
+#     theme_minimal()
+    
+
+
+# Define UI 
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("How many Moose in Fundy National Park"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
+    # Sidebar with a slider input
+            sliderInput("years",
+                        "Year range:",
+                        min = 1993,
+                        max = 2016,
+                        value = c(1993, 2016),
+                        sep = "", width = 600),
 
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
-    )
+        # Show a plot
+           plotOutput("mooseplot", width = 600, height = 600)
+
 )
 
-# Define server logic required to draw a histogram
+# Define server logic required
 server <- function(input, output) {
 
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    output$mooseplot <- renderPlot({
+        moose_data %>%
+            filter(year %in% seq(input$years[1], input$years[2], 1)) %>%
+            ggplot() +
+            geom_bar(aes(x = year, y = total_count), stat = "identity") +
+            ggimage::geom_image(aes(x = year, y = total_count + 4, image = "moose.png"), size = 0.15) +
+            ylab("Number of Moose Spotted") +
+            xlab("Year of Moose Survey") +
+            ggtitle("How many moose seen in Fundy National Park, Canada") +
+            theme_minimal()
     })
 }
 
